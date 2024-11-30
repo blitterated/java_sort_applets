419378df1ca30a074772a8431377be2b972aa23b
blitterated <blitterated@protonmail.com>
2024-11-30 05:26:43 -0600

    Beautified SortItem.java

--- web.archive.org/web/20100902050848/http:/www.cs.ubc.ca/~harrison/Java/SortItem.java
+++ web.archive.org/web/20100902050848/http:/www.cs.ubc.ca/~harrison/Java/SortItem.java
@@ -78,26 +78,26 @@ public class SortItem extends java.applet.Applet implements Runnable {
      * Fill the array with random numbers from 0..n-1.
      */
     void scramble() {
-	int a[] = new int[size().height / 2];
-	double f = size().width / (double) a.length;
-	for (int i = a.length; --i >= 0;) {
+        int a[] = new int[size().height / 2];
+        double f = size().width / (double) a.length;
+        for (int i = a.length; --i >= 0;) {
             a[i] = (int)(a.length * f * Math.random());
-/*jh: fill the array with numbers from 0..n-1, not a scrambled
-  set of 0..n-1 unique numbers.  So duplicates will occur in
-  in most cases */
-/*	    a[i] = (int)(i * f);*/
-	}
+            /*jh: fill the array with numbers from 0..n-1, not a scrambled
+              set of 0..n-1 unique numbers.  So duplicates will occur in
+              in most cases */
+            /*	    a[i] = (int)(i * f);*/
+        }
 
-/*jh: we don't shuffle the array anymore*/
-/*
-	for (int i = a.length; --i >= 0;) {
-	    int j = (int)(i * Math.random());
-	    int t = a[i];
-	    a[i] = a[j];
-	    a[j] = t;
-	}
-        */
-	arr = a;
+        /*jh: we don't shuffle the array anymore*/
+        /*
+        	for (int i = a.length; --i >= 0;) {
+        	    int j = (int)(i * Math.random());
+        	    int t = a[i];
+        	    a[i] = a[j];
+        	    a[j] = t;
+        	}
+                */
+        arr = a;
     }
 
     /**
@@ -105,7 +105,7 @@ public class SortItem extends java.applet.Applet implements Runnable {
      * @see SortAlgorithm
      */
     void pause() {
-	pause(-1, -1);
+        pause(-1, -1);
     }
 
     /**
@@ -113,7 +113,7 @@ public class SortItem extends java.applet.Applet implements Runnable {
      * @see SortAlgorithm
      */
     void pause(int H1) {
-	pause(H1, -1);
+        pause(H1, -1);
     }
 
     /**
@@ -121,27 +121,29 @@ public class SortItem extends java.applet.Applet implements Runnable {
      * @see SortAlgorithm
      */
     void pause(int H1, int H2) {
-	h1 = H1;
-	h2 = H2;
-	if (kicker != null) {
-	    repaint();
-	}
-	try {Thread.sleep(20);} catch (InterruptedException e){}
+        h1 = H1;
+        h2 = H2;
+        if (kicker != null) {
+            repaint();
+        }
+        try {
+            Thread.sleep(20);
+        } catch (InterruptedException e) {}
     }
 
     /**
      * Initialize the applet.
      */
     public void init() {
-	String at = getParameter("alg");
-	if (at == null) {
-	    at = "BubbleSort";
-	}
+        String at = getParameter("alg");
+        if (at == null) {
+            at = "BubbleSort";
+        }
 
-	algName = at + "Algorithm";
-	scramble();
+        algName = at + "Algorithm";
+        scramble();
 
-	resize(100, 100);
+        resize(100, 100);
     }
 
     /**
@@ -149,39 +151,39 @@ public class SortItem extends java.applet.Applet implements Runnable {
      * of horizontal lines of varying lenghts.
      */
     public void paint(Graphics g) {
-	int a[] = arr;
-	int y = size().height - 1;
+        int a[] = arr;
+        int y = size().height - 1;
 
-	// Erase old lines
-	g.setColor(Color.lightGray);
-	for (int i = a.length; --i >= 0; y -= 2) {
-	    g.drawLine(arr[i], y, size().width, y);
-	}
+        // Erase old lines
+        g.setColor(Color.lightGray);
+        for (int i = a.length; --i >= 0; y -= 2) {
+            g.drawLine(arr[i], y, size().width, y);
+        }
 
-	// Draw new lines
-	g.setColor(Color.black);
-	y = size().height - 1;
-	for (int i = a.length; --i >= 0; y -= 2) {
-	    g.drawLine(0, y, arr[i], y);
-	}
+        // Draw new lines
+        g.setColor(Color.black);
+        y = size().height - 1;
+        for (int i = a.length; --i >= 0; y -= 2) {
+            g.drawLine(0, y, arr[i], y);
+        }
 
-	if (h1 >= 0) {
-	    g.setColor(Color.red);
-	    y = h1 * 2 + 1;
-	    g.drawLine(0, y, size().width, y);
-	}
-	if (h2 >= 0) {
-	    g.setColor(Color.blue);
-	    y = h2 * 2 + 1;
-	    g.drawLine(0, y, size().width, y);
-	}
+        if (h1 >= 0) {
+            g.setColor(Color.red);
+            y = h1 * 2 + 1;
+            g.drawLine(0, y, size().width, y);
+        }
+        if (h2 >= 0) {
+            g.setColor(Color.blue);
+            y = h2 * 2 + 1;
+            g.drawLine(0, y, size().width, y);
+        }
     }
 
     /**
      * Update without erasing the background.
      */
     public void update(Graphics g) {
-	paint(g);
+        paint(g);
     }
 
     /**
@@ -192,15 +194,14 @@ public class SortItem extends java.applet.Applet implements Runnable {
      * @see SortItem#mouseUp
      */
     public void run() {
-	try {
-	    if (algorithm == null) {
-		algorithm = (SortAlgorithm)Class.forName(algName).newInstance();
-		algorithm.setParent(this);
-	    }
-	    algorithm.init();
-	    algorithm.sort(arr);
-	} catch(Exception e) {
-	}
+        try {
+            if (algorithm == null) {
+                algorithm = (SortAlgorithm) Class.forName(algName).newInstance();
+                algorithm.setParent(this);
+            }
+            algorithm.init();
+            algorithm.sort(arr);
+        } catch (Exception e) {}
     }
 
     /**
@@ -208,23 +209,22 @@ public class SortItem extends java.applet.Applet implements Runnable {
      * is still sorting.
      */
     public synchronized void stop() {
-	if (kicker != null) {
+        if (kicker != null) {
             try {
-		kicker.stop();
+                kicker.stop();
             } catch (IllegalThreadStateException e) {
                 // ignore this exception
             }
-	    kicker = null;
-	}
-	if (algorithm != null){
+            kicker = null;
+        }
+        if (algorithm != null) {
             try {
-		algorithm.stop();
+                algorithm.stop();
             } catch (IllegalThreadStateException e) {
                 // ignore this exception
             }
-	}
+        }
     }
-
 
     /**
      * For a Thread to actually do the sorting. This routine makes
@@ -234,20 +234,19 @@ public class SortItem extends java.applet.Applet implements Runnable {
      * manipulate the common kicker variable.
      */
     private synchronized void startSort() {
-	if (kicker == null || !kicker.isAlive()) {
-	    scramble();
-	    repaint();
-	    kicker = new Thread(this);
-	    kicker.start();
-	}
+        if (kicker == null || !kicker.isAlive()) {
+            scramble();
+            repaint();
+            kicker = new Thread(this);
+            kicker.start();
+        }
     }
-
 
     /**
      * The user clicked in the applet. Start the clock!
      */
     public boolean mouseUp(java.awt.Event evt, int x, int y) {
-	startSort();
-	return true;
+        startSort();
+        return true;
     }
 }
